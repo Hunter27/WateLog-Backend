3cd1ac5197394d2419aa5cdafc356a8030a13291
dadamsretrorabbit <dadams@retrorabbit.co.za>
2019-01-31 13:26:08 +0200

    Removes parameters.xml
    Changes connectionstring

--- WaterLog_Backend/Startup.cs
+++ WaterLog_Backend/Startup.cs
@@ -21,7 +21,7 @@ namespace WebApplication1
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            _secretConnection = Configuration["DevDatabase:ConnectionString"];
+            _secretConnection = "Server=dev.retrotest.co.za;Database=iot;User Id=group1;Password=fNX^r+UKy3@CtYh5";
         }
 
         public IConfiguration Configuration { get; }
