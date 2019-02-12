1f92dd4597ac6f04d59f616f4642ed1f5b556425
GiftLanga <glanga@retrorabbit.co.za>
2019-02-12 22:06:49 +0200

    created a LinearRegressionModel and controller

---
+++ WaterLog_Backend/Controllers/CostForecastController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WaterLog_Backend.Models;
+
+namespace WaterLog_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CostForecastController : ControllerBase
+    {
+        private readonly DatabaseContext _db;
+        readonly IConfiguration _config;
+        public CostForecastController(DatabaseContext context, IConfiguration config)
+        {
+            _db = context;
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<LinearRegressionModel>> Get([FromBody] LinearRegressionModel data)
+        {
+            Forecast forecast = new Forecast();
+            //get the cost data and dates
+            List<double> y = new List<double>() { 1, 2, 3};
+            DateTime second = new DateTime(2019, 02, 13);
+            //-----------------------------
+
+            var epochDates = forecast.generateUnixEpochFromDatetime(data.start, data.end, second.Subtract(data.start));
+
+            double rSquared, yIntercept, slope;
+
+            forecast.LinearRegression(epochDates.ToArray(), y.ToArray(), out rSquared, out yIntercept, out slope);
+
+            data.rSquared = rSquared;
+            data.yIntercept = yIntercept;
+            data.slope = slope;
+
+            return data;
+        }
+    }
+}
---
+++ WaterLog_Backend/Models/LinearRegressionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaterLog_Backend.Models
+{
+    public class LinearRegressionModel
+    {
+        public double yIntercept { get; set; }
+        public double slope { get; set; }
+        public double rSquared { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+    }
+}
