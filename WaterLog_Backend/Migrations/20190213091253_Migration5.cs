00b899563f1716ca4ab7b0273a25b87d0fcb5ba1
lisaQ04 <lquluba@retrorabbit.co.za>
2019-02-13 11:16:06 +0200

    Created New Migration and Pumps table

---
+++ WaterLog_Backend/Migrations/20190213091253_Migration5.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WaterLog_Backend.Migrations
+{
+    public partial class Migration5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pumps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pumps", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pumps");
+        }
+    }
+}
---
+++ WaterLog_Backend/Models/PumpEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaterLog_Backend.Models
+{
+    public class PumpEntry
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+    }
+}
