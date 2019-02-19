47832b599cbb45fdb10ef5d7bf2bc4520fd4725a
unknown <amathenjwa@retrorabbit.co.za>
2019-02-19 14:52:40 +0200

    Add closing {}

--- WaterLog_Backend/Controllers/SegmentEventsController.cs
+++ WaterLog_Backend/Controllers/SegmentEventsController.cs
@@ -154,6 +154,7 @@ namespace WaterLog_Backend.Controllers
             {
                 throw error;
             }
+        }
         [Route("dailyUsage")]
         public async Task<DataPoints<DateTime, double>> GetDailyUsgaeGraphData()
         {
