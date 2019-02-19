2f82144ab1788d5a8fc77fd06a4f9ce9c90652ba
NombusoRR <nsibiya@retrorabbit.co.za>
2019-02-19 14:45:28 +0200

    Merge branch 'Dev' of https://github.com/Hunter27/WaterLog-Back-End into feature/ResolvingEvent
    # Conflicts:
    #	WaterLog_Backend/Models/DatabaseContext.cs

--- WaterLog_Backend/Models/DatabaseContext.cs
+++ WaterLog_Backend/Models/DatabaseContext.cs
@@ -20,6 +20,7 @@ namespace WaterLog_Backend.Models
         public DbSet<MonitorsEntry> Monitors { get; set; }
         public DbSet<ReadingsEntry> Readings { get; set; }
         public DbSet<SegmentsEntry> Segments { get; set; }
+        public DbSet<SegmentEventsEntry> SegmentEvents { get; set; }
         public virtual DbSet<SegmentLeaksEntry> SegmentLeaks { get; set; } 
         public DbSet<PumpEntry> Pumps { get; set; }
         public DbSet<HistoryLogEntry> HistoryLogs { get; set; }
