e40d293157f68147a2adce9f456802144e326b27
Hunter27 <nmotsumi@retrorabbit.co.za>
2019-02-20 15:50:23 +0200

    finished protocol from IOT devices to trigger when theres a leak

--- WaterLog_Backend/Controllers/ReadingsController.cs
+++ WaterLog_Backend/Controllers/ReadingsController.cs
@@ -69,6 +69,8 @@ namespace WaterLog_Backend.Controllers
             await _db.SaveChangesAsync();
             await _db.Readings.AddAsync(reading2);
             await _db.SaveChangesAsync();
+            Procedures procedure = new Procedures(_db, _config);
+            await procedure.TriggerInsert(reading);
         }
 
         // PUT api/readings/
