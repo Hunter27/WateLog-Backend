187f9b28e4fcad114655d034223c14f4de3d0d2c
GiftLanga <glanga@retrorabbit.co.za>
2019-02-20 19:40:15 +0200

    created monthly forecast for bar graph

--- WaterLog_Backend/Controllers/CostForecastController.cs
+++ WaterLog_Backend/Controllers/CostForecastController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using WaterLog_Backend.Models;
 
@@ -57,39 +58,38 @@ namespace WaterLog_Backend.Controllers
             return data;
         }
 
-        [Route("monthly")]
+        [Route("monthly/{id}")]
         [HttpGet]
-        public async Task<ActionResult<LinearRegressionModel>> GenerateLinearMonthlyCostForecast()
+        public async Task<ActionResult<double>> GetMonthCostForecast(int id)
         {
-            Forecast forecast = new Forecast();
-            //get the cost data and dates
-            Procedures proc = new Procedures(_db, _config);
-            var results = await Task.Run(() => proc.CalculatePeriodWastageAsync(Procedures.Period.Monthly).Result[0]);
+            Procedures P = new Procedures();
+            var thisMonthsEvents = P.sumamryDailyCost( await _db
+                    .SegmentEvents.Where(a => a.TimeStamp.Month == id && a.TimeStamp.Year == DateTime.Now.Year)
+                    .GroupBy(b => b.TimeStamp.Hour)
+                    .ToListAsync()).FirstOrDefault();
 
-            if (results.dataPoints.Count < 2)
+            if (thisMonthsEvents.dataPoints.Count < 3)
             {
-                throw new ArgumentException("data points should be at least 2");
+                return new BadRequestObjectResult("Not Possible"); ;
             }
-            var x = results.dataPoints.Select(row => row.x); //datetime data
-            var y = results.dataPoints.Select(row => row.y); //cost data <double>
+            var orderedEvents = thisMonthsEvents.dataPoints.OrderBy(d => d.x.Date);
+            var x = (orderedEvents.ToArray()).Select(row => row.x); //datetime data
+            var y = (orderedEvents.ToArray()).Select(row => row.y); //cost data <double>
 
             DateTime start = x.ElementAt(0);
             DateTime second = x.ElementAt(1);
             DateTime end = x.Last();
 
-            var epochDates = forecast.generateUnixEpochFromDatetime(start, end, x.Count());
+            Forecast forecast = new Forecast();
+            var epochDates = forecast.generateUnixEpochFromDatetime(start, end, y.Count());
 
             double rSquared, yIntercept, slope;
 
             forecast.LinearRegression(epochDates.ToArray(), y.ToArray(), out rSquared, out yIntercept, out slope);
-
-            LinearRegressionModel data = new LinearRegressionModel();
-            data.rSquared = rSquared;
-            data.yIntercept = yIntercept;
-            data.slope = slope;
-            data.start = epochDates[0];
-
-            return data;
+            var day = DateTime.DaysInMonth(DateTime.Now.Year, id);
+            var date = new DateTime(DateTime.Now.Year, id, day);
+            
+            return slope*(new DateTimeOffset(date).ToUnixTimeSeconds()) + yIntercept;
         }
 
         [Route("seasonal")]
