d9a00e0fff991abdc5cb0166e3298543ead6e780
lisaQ04 <lquluba@retrorabbit.co.za>
2019-02-25 14:50:32 +0200

    Sensor history model

---
+++ WaterLog_Backend/Models/SensorHistoryEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaterLog_Backend.Models
+{
+    public class SensorHistoryEntry
+    {
+        public int Id { get; set; }
+        public int SensorId { get; set; }
+        public EnumResolveStatus SensorResolved { get; set; }
+        public DateTime FaultDate { get; set; }
+        public DateTime AttendedDate { get; set; }
+        public DateTime EmailSentDate { get; set; } 
+        public EnumSensorType SensorType { get; set; }
+    }
+}
