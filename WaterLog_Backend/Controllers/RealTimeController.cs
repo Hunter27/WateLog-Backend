e74c1d3928a2bc802f14982c0152d533b1b78efc
dadamsretrorabbit <dadams@retrorabbit.co.za>
2019-02-27 15:16:27 +0200

    Introduces Backend Poll Notifications Endpoint.

---
+++ WaterLog_Backend/Controllers/RealTimeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WaterLog_Backend.Models;
+
+namespace WaterLog_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RealTimeController : ControllerBase
+    {
+        private readonly DatabaseContext _db;
+        readonly IConfiguration _config;
+        public RealTimeController(DatabaseContext context, IConfiguration config)
+        {
+            _db = context;
+            _config = config;
+        }
+
+        [Route("pollnotifications")]
+        public async Task<int> GetLatestNumberNotifications()
+        {
+            var leaksCount = await _db.SegmentLeaks.Where(b => b.ResolvedStatus == EnumResolveStatus.UNRESOLVED).CountAsync();
+            var faultyCount = await _db.SensorHistory.Where(b => b.SensorResolved == EnumResolveStatus.UNRESOLVED).CountAsync();
+
+            return (leaksCount + faultyCount);
+        }
+    }
+}
